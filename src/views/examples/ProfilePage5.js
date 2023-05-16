6879ef297f7699b7fc719a7db70166f542048835
GeffersonST <83526000+GeffersonST@users.noreply.github.com>
2023-05-16 16:21:15 -0300

    Estruturação do Ava-Hub

---
+++ src/views/examples/ProfilePage5.js
@@ -0,0 +1,461 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import classnames from "classnames";
+// javascript plugin used to create scrollbars on windows
+import PerfectScrollbar from "perfect-scrollbar";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Label,
+  FormGroup,
+  Form,
+  Input,
+  FormText,
+  NavItem,
+  NavLink,
+  Nav,
+  Table,
+  TabContent,
+  TabPane,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+  UncontrolledCarousel,
+} from "reactstrap";
+
+// core components
+
+import Footer from "components/Footer/Footer.js";
+import IndexNavbar from "components/Navbars/IndexNavbar";
+
+const carouselItems = [
+  {
+    src: require("assets/img/guanacurso1.jpg"),
+    altText: "Slide 1",
+    caption: "Big City Life, United States",
+  },
+  {
+    src: require("assets/img/guanacurso2.jpeg"),
+    altText: "Slide 2",
+    caption: "Somewhere Beyond, United States",
+  },
+  {
+    src: require("assets/img/guanacurso3.png"),
+    altText: "Slide 3",
+    caption: "Stocks, United States",
+  },
+];
+
+let ps = null;
+
+export default function ProfilePage() {
+  const [tabs, setTabs] = React.useState(1);
+  React.useEffect(() => {
+    if (navigator.platform.indexOf("Win") > -1) {
+      document.documentElement.className += " perfect-scrollbar-on";
+      document.documentElement.classList.remove("perfect-scrollbar-off");
+      let tables = document.querySelectorAll(".table-responsive");
+      for (let i = 0; i < tables.length; i++) {
+        ps = new PerfectScrollbar(tables[i]);
+      }
+    }
+    document.body.classList.toggle("profile-page");
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      if (navigator.platform.indexOf("Win") > -1) {
+        ps.destroy();
+        document.documentElement.className += " perfect-scrollbar-off";
+        document.documentElement.classList.remove("perfect-scrollbar-on");
+      }
+      document.body.classList.toggle("profile-page");
+    };
+  }, []);
+  return (
+    <>
+      <IndexNavbar />
+      <div className="wrapper">
+        <div className="page-header">
+          <img
+            alt="..."
+            className="dots"
+            src={require("assets/img/dots.png")}
+          />
+          <img
+            alt="..."
+            className="path"
+            src={require("assets/img/path4.png")}
+          />
+          <Container className="align-items-center">
+            <Row>
+              <Col lg="6" md="6">
+                <h1 className="profile-title text-left">Gustavo Guanabara</h1>
+                <h5 className="text-on-back">01</h5>
+                <p className="profile-description">
+                  Gustavo Guanabara é professor carioca desde 1994 e já atuou em
+                  todo tipo de segmento. Atuou desde ensino infantil, passando
+                  por cursos técnicos, universidades e já coordenou o setor de
+                  tutoria para ensino à distância de uma universidade pública.
+                  Atualmente, viaja pelo Brasil dando palestras em eventos
+                  nacionais e internacionais, como Campus Party, FISL,
+                  Latinoware, etc.
+                </p>
+                <div className="btn-wrapper profile pt-3">
+                  <Button
+                    className="btn-icon btn-round"
+                    color="twitter"
+                    href="https://twitter.com/creativetim"
+                    id="tooltip639225725"
+                    target="_blank"
+                  >
+                    <i className="fab fa-twitter" />
+                  </Button>
+                  <UncontrolledTooltip delay={0} target="tooltip639225725">
+                    Follow us
+                  </UncontrolledTooltip>
+                  <Button
+                    className="btn-icon btn-round"
+                    color="facebook"
+                    href="https://www.facebook.com/creativetim"
+                    id="tooltip982846143"
+                    target="_blank"
+                  >
+                    <i className="fab fa-facebook-square" />
+                  </Button>
+                  <UncontrolledTooltip delay={0} target="tooltip982846143">
+                    Like us
+                  </UncontrolledTooltip>
+                  <Button
+                    className="btn-icon btn-round"
+                    color="dribbble"
+                    href="https://dribbble.com/creativetim"
+                    id="tooltip951161185"
+                    target="_blank"
+                  >
+                    <i className="fab fa-dribbble" />
+                  </Button>
+                  <UncontrolledTooltip delay={0} target="tooltip951161185">
+                    Follow us
+                  </UncontrolledTooltip>
+                </div>
+              </Col>
+              <Col className="ml-auto mr-auto" lg="4" md="6">
+                <Card className="card-coin card-plain">
+                  <CardHeader>
+                    <img
+                      alt="..."
+                      className="img-center img-fluid rounded-circle"
+                      src={require("assets/img/guana2.jpg")}
+                    />
+                    <h4 className="title">Alguns números</h4>
+                  </CardHeader>
+                  <CardBody>
+                    <Nav
+                      className="nav-tabs-primary justify-content-center"
+                      tabs
+                    >
+                      <NavItem>
+                        <NavLink
+                          className={classnames({
+                            active: tabs === 1,
+                          })}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setTabs(1);
+                          }}
+                          href="#pablo"
+                        >
+                          Alunos
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink
+                          className={classnames({
+                            active: tabs === 2,
+                          })}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setTabs(2);
+                          }}
+                          href="#pablo"
+                        >
+                          Send
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink
+                          className={classnames({
+                            active: tabs === 3,
+                          })}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setTabs(3);
+                          }}
+                          href="#pablo"
+                        >
+                          News
+                        </NavLink>
+                      </NavItem>
+                    </Nav>
+                    <TabContent
+                      className="tab-subcategories"
+                      activeTab={"tab" + tabs}
+                    >
+                      <TabPane tabId="tab1">
+                        <Table className="tablesorter" responsive>
+                          <thead className="text-primary">
+                            <tr>
+                              <th className="header">COIN</th>
+                              <th className="header">AMOUNT</th>
+                              <th className="header">VALUE</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>BTC</td>
+                              <td>7.342</td>
+                              <td>48,870.75 USD</td>
+                            </tr>
+                            <tr>
+                              <td>ETH</td>
+                              <td>30.737</td>
+                              <td>64,53.30 USD</td>
+                            </tr>
+                            <tr>
+                              <td>XRP</td>
+                              <td>19.242</td>
+                              <td>18,354.96 USD</td>
+                            </tr>
+                          </tbody>
+                        </Table>
+                      </TabPane>
+                      <TabPane tabId="tab2">
+                        <Row>
+                          <Label sm="3">Pay to</Label>
+                          <Col sm="9">
+                            <FormGroup>
+                              <Input
+                                placeholder="e.g. 1Nasd92348hU984353hfid"
+                                type="text"
+                              />
+                              <FormText color="default" tag="span">
+                                Please enter a valid address.
+                              </FormText>
+                            </FormGroup>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Label sm="3">Amount</Label>
+                          <Col sm="9">
+                            <FormGroup>
+                              <Input placeholder="1.587" type="text" />
+                            </FormGroup>
+                          </Col>
+                        </Row>
+                        <Button
+                          className="btn-simple btn-icon btn-round float-right"
+                          color="primary"
+                          type="submit"
+                        >
+                          <i className="tim-icons icon-send" />
+                        </Button>
+                      </TabPane>
+                      <TabPane tabId="tab3">
+                        <Table className="tablesorter" responsive>
+                          <thead className="text-primary">
+                            <tr>
+                              <th className="header">Latest Crypto News</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>The Daily: Nexo to Pay on Stable...</td>
+                            </tr>
+                            <tr>
+                              <td>Venezuela Begins Public of Nation...</td>
+                            </tr>
+                            <tr>
+                              <td>PR: BitCanna – Dutch Blockchain...</td>
+                            </tr>
+                          </tbody>
+                        </Table>
+                      </TabPane>
+                    </TabContent>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="section">
+          <Container>
+            <Row className="justify-content-between">
+              <Col md="6">
+                <Row className="justify-content-between align-items-center">
+                  <UncontrolledCarousel items={carouselItems} />
+                </Row>
+              </Col>
+              <Col md="5">
+                <h1 className="profile-title text-left">Projetos</h1>
+                <h5 className="text-on-back">02</h5>
+                <p className="profile-description text-left">
+                  Lançado em 2013, o projeto de ensinar tecnologia à distância
+                  foi ganhando forma e hoje já conta com milhares de alunos
+                  inscritos e uma grande quantidade de material gratuito. O
+                  professor carioca Gustavo Guanabara sempre buscou criar aulas
+                  diferentes dentro da sua sala. Quando descobriu que poderia
+                  dar aulas para milhares de pessoas pela internet, criou seu
+                  primeiro curso de PHP em 2005. Desde aquela época, já teve
+                  mais de 8 milhões de downloads e colaborou para a formação de
+                  muitos programadores iniciantes. Em 2013, com o apoio
+                  principal da Hostnet, Guanabara iniciou seu projeto mais
+                  ambicioso: ensinar tecnologia através de um site que
+                  disponibilizasse cursos completos em vídeo, com novas aulas
+                  lançadas semanalmente. Ainda em 2013, foi lançado o Curso em
+                  Vídeo de HTML5 e no início de 2014, iniciou o Curso em Vídeo
+                  de Algoritmo. Ambos 100% gratuitos.
+                </p>
+                <div className="btn-wrapper pt-3">
+                  <Button
+                    className="btn-simple"
+                    color="primary"
+                    href="#pablo"
+                    onClick={(e) => e.preventDefault()}
+                  >
+                    <i className="tim-icons icon-book-bookmark" /> Bookmark
+                  </Button>
+                  <Button
+                    className="btn-simple"
+                    color="info"
+                    href="#pablo"
+                    onClick={(e) => e.preventDefault()}
+                  >
+                    <i className="tim-icons icon-bulb-63" /> Check it!
+                  </Button>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <section className="section">
+          <Container>
+            <Row>
+              <Col md="6">
+                <Card className="card-plain">
+                  <CardHeader>
+                    <h1 className="profile-title text-left">Contact</h1>
+                    <h5 className="text-on-back">03</h5>
+                  </CardHeader>
+                  <CardBody>
+                    <Form>
+                      <Row>
+                        <Col md="6">
+                          <FormGroup>
+                            <label>Your Name</label>
+                            <Input defaultValue="Mike" type="text" />
+                          </FormGroup>
+                        </Col>
+                        <Col md="6">
+                          <FormGroup>
+                            <label>Email address</label>
+                            <Input placeholder="mike@email.com" type="email" />
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col md="6">
+                          <FormGroup>
+                            <label>Phone</label>
+                            <Input defaultValue="001-12321345" type="text" />
+                          </FormGroup>
+                        </Col>
+                        <Col md="6">
+                          <FormGroup>
+                            <label>Company</label>
+                            <Input defaultValue="CreativeTim" type="text" />
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col md="12">
+                          <FormGroup>
+                            <label>Message</label>
+                            <Input placeholder="Hello there!" type="text" />
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                      <Button
+                        className="btn-round float-right"
+                        color="primary"
+                        data-placement="right"
+                        id="tooltip341148792"
+                        type="button"
+                      >
+                        Send text
+                      </Button>
+                      <UncontrolledTooltip
+                        delay={0}
+                        placement="right"
+                        target="tooltip341148792"
+                      >
+                        Can't wait for your message
+                      </UncontrolledTooltip>
+                    </Form>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col className="ml-auto" md="4">
+                <div className="info info-horizontal">
+                  <div className="icon icon-primary">
+                    <i className="tim-icons icon-square-pin" />
+                  </div>
+                  <div className="description">
+                    <h4 className="info-title">Find us at the office</h4>
+                    <p>
+                      Bld Mihail Kogalniceanu, nr. 8, <br />
+                      7652 Bucharest, <br />
+                      Romania
+                    </p>
+                  </div>
+                </div>
+                <div className="info info-horizontal">
+                  <div className="icon icon-primary">
+                    <i className="tim-icons icon-mobile" />
+                  </div>
+                  <div className="description">
+                    <h4 className="info-title">Give us a ring</h4>
+                    <p>
+                      Michael Jordan <br />
+                      +40 762 321 762 <br />
+                      Mon - Fri, 8:00-22:00
+                    </p>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        <Footer />
+      </div>
+    </>
+  );
+}
